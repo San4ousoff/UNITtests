5a5661889b2989323c831f4edc6e41d69c9c742e
Kiriakov Aleksandr <san4ousoff@mail.ru>
2023-09-27 15:59:12 +0200

    Class Work Seminar1

---
+++ UnitTests/src/main/java/Seminar1/CW/task8/MyClass.java
@@ -0,0 +1,54 @@
+package Seminar1.CW.task8;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyClass {
+    public static List<Integer> sumLists(List<Integer> list1, List<Integer> list2) {
+        if (list1.size() != list2.size()) {
+            throw new IllegalArgumentException("Списки должны быть одинаковой длины");
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < list1.size(); i++) {
+            result.add(list1.get(i) + list2.get(i));
+        }
+
+        return result;
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException("Список не должен быть пустым");
+        }
+
+        int max = numbers.get(0);
+
+        for (int num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return max;
+    }
+
+    public static List<String> filterStrings(List<String> words, int minLength) {
+        if (minLength < 0) {
+            throw new IllegalArgumentException("Минимальная длина не может быть отрицательной");
+        }
+
+        return words.stream()
+                .filter(word -> word.length() >= minLength)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("один", "два", "три", "четыре", "пять", "шесть");
+        List<String> filteredWords1 = filterStrings(words, 5);
+        System.out.println(filteredWords1);
+    }
+}
---
+++ UnitTests/src/test/java/Seminar1/CW/task1/MyTest.java
@@ -0,0 +1,10 @@
+package Seminar1.CW.task1;
+
+public class MyTest {
+    public static void main(String[] args) {
+        int x = -1;
+        // проверяем, что х неотрицательно с использованием оператора assert
+        assert x >= 0 : "x должно быть неотрицательным";
+        System.out.println("Программа продолжает выполнение");
+    }
+}
